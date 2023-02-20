6c1badda638a48309afc1da73f1995dc5999edff
Mohammad Burhan <104616403+BurhanMohammad@users.noreply.github.com>
2023-02-20 15:18:38 +0530

    updated

--- final_project/index.js
+++ final_project/index.js
@@ -2,18 +2,78 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
+//const authenticatedUser = require('./router/auth_users.js').authenticatedUser;
 const genl_routes = require('./router/general.js').general;
 
-const app = express();
+/*let user = []
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+   }
+   
+   const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+   }*/
+   
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
+
 //Write the authenication mechanism here
-});
- 
+app.use("/customer/auth/*", function auth(req,res,next){
+/*	const username = req.body.username;
+	const password = req.body.password;
+	if (!username || !password) {
+		return res.status(404).json({message: "Error logging in"});
+	}
+	if (authenticatedUser (username, password)) {
+		let accessToken = jwt.sign({
+			data: password
+		}, 'access', {expiresIn: 60 * 60});
+		
+		req.session.autorization = {
+			accessToken, username
+		}
+	return res.status(200).send("User success login");
+		} else
+		{
+			return res.status(208).json({message: "Invalid Lagin. Check username and password "});
+		}
+    });*/
+    if (req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+          if (!err) {
+            req.user = user;
+            next();
+          } else {
+            return res.status(403).json({message: "User not authenticated"});
+          }
+        });
+      } else {
+        return res.status(403).json({message: "User not logged in"});
+      }
+    });
+
+    
+
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
